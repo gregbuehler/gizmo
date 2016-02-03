b067c55a41e25d51384def46ffbcbb213c4f838f
Greg Buehler <gregory.r.buehler@gmail.com>
2016-02-02 16:11:56 -0800

    Add present or absent state for package command generation

--- lib/modules/package.rb
+++ lib/modules/package.rb
@@ -3,7 +3,12 @@ require_relative "./base"
 class PackageModule < BaseModule
   def execute
     puts "\tModule: package"
-    command = "sudo apt-get install #{@options['name']}"
+    state = ""
+    state = "install" if @options['state'].downcase === "present"
+    state = "remove" if @options['state'].downcase === "absent"
+    
+    command = "sudo apt-get #{state} #{@options['name']}"
+
     puts "\t\tRaw Command: #{command}"
   end
 end
